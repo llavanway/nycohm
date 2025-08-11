8ab7dd47cb0389a13d409ecd5b07133fc5cea50f
Luke Lavanway <luke.lavanway@gmail.com>
2025-08-11 13:59:00 -0400

    Fix Dagster integration and update pipeline

--- src/nycohm/definitions.py
+++ src/nycohm/definitions.py
@@ -1,6 +1,7 @@
 from dagster import Definitions, define_asset_job, load_assets_from_modules
+
 from . import assets
-from .helpers.connect_bq import connect_bq
+from .helpers.connect_bq import bq_client
 
 all_assets = load_assets_from_modules([assets])
 nyc_housing_job = define_asset_job("nyc_housing_job")
@@ -8,5 +9,5 @@ nyc_housing_job = define_asset_job("nyc_housing_job")
 defs = Definitions(
     assets=all_assets,
     jobs=[nyc_housing_job],
-    resources={"bq_client": connect_bq},
+    resources={"bq_client": bq_client},
 )
